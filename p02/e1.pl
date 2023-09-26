07e967d142b49c3ee087ee3f787cb5f685625f59
Pedro Gomes <up202108825@up.pt>
2023-09-26 10:58:16 +0100

    p02e1 done

---
+++ p02/e1.pl
@@ -0,0 +1,19 @@
+r(a, b).
+r(a, d).
+r(b, a).
+r(a, c).
+s(b, c).
+s(b, d).
+s(c, c).
+s(d, e).
+
+% ex 1a
+% i) X = a, Y = b, Z = c
+% ii) Y = c, X = a
+% iii) X = a, Y = c
+
+% ex 1b
+% i) 0
+% ii) 0
+% iii) 3
+
