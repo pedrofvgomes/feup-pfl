3ab599c311c9cf734bb0608e6228cc1ed2c851e4
Pedro Gomes <up202108825@up.pt>
2023-10-28 18:58:00 +0100

    draw_board

--- project/draw.pl
+++ project/draw.pl
@@ -1,2 +1,14 @@
 draw_title:-
     write('title'),nl.
+
+draw_board([]).
+draw_board([Line | Lines]):-
+    draw_line(Line),
+    nl,
+    draw_board(Lines).
+    
+draw_line([]).
+draw_line([A|B]) :-
+  write(A),
+  write(' '),
+  draw_line(B).
