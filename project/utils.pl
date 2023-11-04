9b21763b7f05454bc107e9fe5921bb2d05a83c7d
Pedro Gomes <up202108825@up.pt>
2023-11-04 20:03:37 +0000

    board_index

--- project/utils.pl
+++ project/utils.pl
@@ -3,3 +3,11 @@ list_size([_|T], Size):-
         list_size(T, Newsize),
         Size is Newsize+1.
 
+list_index([A|_], 0, A).
+list_index([_|B], Index, Item):-
+        (Index > 0 -> 
+                I1 is Index-1, list_index(B,I1,Item); !).
+
+board_index([A|B], X, Y, Item):-
+        list_index([A|B], Y, Row),
+        list_index(Row, X, Item).
