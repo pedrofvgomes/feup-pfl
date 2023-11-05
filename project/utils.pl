25f895e5012b6d9a2949155f91f2ae07c4355318
Pedro Gomes <up202108825@up.pt>
2023-11-05 00:20:25 +0000

    update_board

--- project/utils.pl
+++ project/utils.pl
@@ -18,3 +18,9 @@ update_list([A|B1], Index, Value, [A|B2]) :-
         Index > 0,
         I1 is Index - 1,
         update_list(B1, I1, Value, B2).
+
+update_board([A1|B1], X, Y, Value, [A2|B2]) :-
+        list_index([A1|B1], Y, Row),   
+        update_list(Row, X, Value, NewRow),  
+        update_list([A1|B1], Y, NewRow, [A2|B2]). 
+        
