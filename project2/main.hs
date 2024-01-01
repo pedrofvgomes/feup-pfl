0da70811f3157de8d52f279d31290e1d9882e53e
Pedro Gomes <up202108825@up.pt>
2024-01-01 20:44:28 +0000

    removed unnecessary file

--- project2/main.hs
+++ project2/main.hs
@@ -1,49 +1,55 @@
 import Data.List (sort, intercalate)
+import Data.Char (isDigit)
 
+-- PFL 2023/24 - Haskell practical assignment quickstart
+-- Updated on 27/12/2023
+
+-- Part 1
+
+-- Do not modify our definition of Inst and Code
 data Inst =
   Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | Store String | Noop |
   Branch Code Code | Loop Code Code
   deriving Show
-
 type Code = [Inst]
-type Stack = [Integer]
-type Storage = [(String, Integer)]
-type State = (Stack, Storage)
+
+-- storing as a list of strings (either numeric strings or boolean strings, to be converted later)
+type Stack = [String]
+
+-- storing as a list of pairs of strings (either numeric strings or boolean strings, to be converted later)
+type State = [(String, String)]
 
 createEmptyStack :: Stack
 createEmptyStack = []
 
-stack2Str :: Stack -> String
-stack2Str stack = intercalate ", " (map showConverted stack)
-  where
-    showConverted :: Integer -> String
-    showConverted x
-      | x == 111 = "True"
-      | x == 999 = "False"
-      | otherwise = show x
-
 createEmptyState :: State
-createEmptyState = ([], [])
+createEmptyState = []
+
+stack2Str :: Stack -> String
+stack2Str stack = intercalate "," stack
 
 state2Str :: State -> String
-state2Str state = intercalate ", " [var ++ "=" ++ showWithBoolean val | (var, val) <- sort state]
-  where
-    showWithBoolean x = case x of
-      111 -> "True"
-      999 -> "False"
-      _ -> show x
+state2Str = intercalate "," . map (\(var, val) -> var ++ "=" ++ val) . sort
 
 run :: (Code, Stack, State) -> (Code, Stack, State)
 run ([], stack, state) = ([], stack, state)
 run (inst:insts, stack, state) = case inst of
-  Push n -> run (insts, n:stack, state)
+  Push n -> run (insts, (show n):stack, state)
   Add -> applyBinaryOperation (+)
   Mult -> applyBinaryOperation (*)
   Sub -> applyBinaryOperation (-)
-  Tru -> pushValue 111
-  Fals -> pushValue 999
-  Equ -> applyComparisonOperation (==)
-  Le -> applyComparisonOperation (<=)
+  Tru -> pushValue "True"
+  Fals -> pushValue "False"
+  Equ -> case stack of 
+    x:y:rest -> if (elem x ["True", "False"] && elem y ["True", "False"]) || (all isDigit x && all isDigit y || x !! 0 == '-' && all isDigit (drop 1 x) && y !! 0 == '-' && all isDigit (drop 1 y))
+      then run (insts, (show (x == y)):rest, state)
+      else error "Run-time error"
+    _ -> error "Run-time error"
+  Le -> case stack of
+    x:y:rest -> if all isDigit x && all isDigit y || x !! 0 == '-' && all isDigit (drop 1 x) && y !! 0 == '-' && all isDigit (drop 1 y)
+      then run (insts, (show ((read x :: Integer) <= (read y :: Integer))):rest, state)
+      else error "Run-time error"
+    _ -> error "Run-time error"
   And -> applyLogicOperation (&&)
   Neg -> applyNegationOperation
   Fetch var -> runFetch var
@@ -54,90 +60,96 @@ run (inst:insts, stack, state) = case inst of
   where
     -- Funções auxiliares para as instruções
     applyBinaryOperation op = case stack of
-      x:y:rest -> run (insts, (op y x):rest, state)
-      _ -> error "Run-time error: Operation requires at least two elements on the stack"
-
-    applyComparisonOperation op = case stack of
-      x:y:rest -> if x `op` y then run (insts, 111:rest, state) else run (insts, 999:rest, state)
-      _ -> error "Run-time error: Comparison operation requires at least two elements on the stack"
+      x:y:rest -> run (insts, (show (op (read x) (read y))):rest, state)
+      _ -> error "Run-time error"
 
     applyLogicOperation op = case stack of
-      x:y:rest -> if x == 111 && y == 111 then run (insts, 111:rest, state) else run (insts, 999:rest, state)
-      _ -> error "Run-time error: Logic operation requires at least two elements on the stack"
+      x:y:rest -> run (insts, (show (op (read x) (read y))):rest, state)
+      _ -> error "Run-time error"
 
     applyNegationOperation = case stack of
-      x:rest -> run (insts, if x == 111 then 999:rest else 111:rest, state)
-      _ -> error "Run-time error: Negation operation requires at least one element on the stack"
+      x:rest -> run (insts, (show (not (read x))):rest, state)
+      _ -> error "Run-time error"
 
-    runFetch var = case lookup var (snd state) of
+    pushValue val = run (insts, val:stack, state)
+
+    runFetch var = case lookup var state of
       Just val -> run (insts, val:stack, state)
-      Nothing -> error $ "Run-time error: Variable " ++ var ++ " not found in storage"
+      Nothing -> error "Run-time error"
 
-    runStore var = case stack of
-      val:rest -> run (insts, rest, (var, val):(snd state))
-      _ -> error "Run-time error: Store operation requires at least one element on the stack"
+    runStore var =
+      case stack of
+        val:rest ->
+          case lookup var state of
+            Just _  -> run (insts, rest, updateState var val state)
+            Nothing -> run (insts, rest, (var, val):state)
+        _ -> error "Run-time error"
+      where
+        updateState :: String -> String -> State -> State
+        updateState key newVal [] = [(key, newVal)]
+        updateState key newVal ((k, v):rest)
+          | key == k  = (key, newVal) : rest
+          | otherwise = (k, v) : updateState key newVal rest
 
     runBranch c1 c2 = case stack of
-      111:rest -> run (c1, rest, state)
-      999:rest -> run (c2, rest, state)
-      _ -> error "Run-time error: Branch expects True (111) or False (999) on the stack"
+      val:rest -> if val == "True" then run (c1 ++ insts, rest, state) else run (c2 ++ insts, rest, state)
+      _ -> error "Run-time error"
 
-    runLoop c1 c2 = case stack of
-      111:rest -> run (c1 ++ [Branch c2 [Loop c1 c2]], rest, state)
-      999:rest -> run (Noop:insts, rest, state)
-      _ -> error "Run-time error: Loop expects True (111) or False (999) on the stack"
+    runLoop c1 c2 = run (c1 ++ [Branch (c2 ++ [Loop c1 c2]) [Noop]] ++ insts, stack, state)
 
+-- To help you test your assembler
 testAssembler :: Code -> (String, String)
 testAssembler code = (stack2Str stack, state2Str state)
-  where (_, stack, state) = run (code, createEmptyStack, createEmptyState)
+  where (_,stack,state) = run(code, createEmptyStack, createEmptyState)
 
+-- Examples:
+-- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
+-- testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
+-- testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
+-- testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
+-- testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
+-- testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
+-- testAssembler [Push (-20),Push (-21), Le] == ("True","")
+-- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
+-- testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
+-- If you test:
+-- testAssembler [Push 1,Push 2,And]
+-- You should get an exception with the string: "Run-time error"
+-- If you test:
+-- testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
+-- You should get an exception with the string: "Run-time error"
 
-main :: IO ()
-main = do
-  let code1 = [Push 5, Push 3, Add]
-      result1 = testAssembler code1
-  putStrLn $ show result1
+-- Part 2
 
-  let code2 = [Tru, Tru, Or]
-      result2 = testAssembler code2
-  putStrLn $ show result2
+-- TODO: Define the types Aexp, Bexp, Stm and Program
 
-  let code3 = [Fals, Fals, Not]
-      result3 = testAssembler code3
-  putStrLn $ show result3
+-- compA :: Aexp -> Code
+compA = undefined -- TODO
 
-  -- Simulating a run-time error
-  let code4 = [Push 10, Push 5, Le, Fetch "invalidVar"]
-      result4 = testAssembler code4
-  putStrLn $ show result4
+-- compB :: Bexp -> Code
+compB = undefined -- TODO
 
-  let code5 = [Push 50, Push 100, Le]
-      result5 = testAssembler code5
-  putStrLn $ show result5
+-- compile :: Program -> Code
+compile = undefined -- TODO
 
-  let code6 = [Push 4, Push 4, Equ]
-      result6 = testAssembler code6
-  putStrLn $ show result6
+-- parse :: String -> Program
+parse = undefined -- TODO
 
-  let code7 = [Push 7, Push 8, Equ]
-      result7 = testAssembler code7
-  putStrLn $ show result7
+-- To help you test your parser
+testParser :: String -> (String, String)
+testParser programCode = (stack2Str stack, state2Str state)
+  where (_,stack,state) = run(compile (parse programCode), createEmptyStack, createEmptyState)
 
-  let code8 = [Push 7, Push 8, Equ, Neg]
-      result8 = testAssembler code8
-  putStrLn $ show result8
-
-  let code9 = [Push 7, Push 8, Equ, Neg, Neg]
-      result9 = testAssembler code9
-  putStrLn $ show result9
-
-  let code10 = [Push 7, Push 7, Equ, Equ]
-      result10 = testAssembler code10
-  putStrLn $ show result10
-
-
-
-
-
-
-
+-- Examples:
+-- testParser "x := 5; x := x - 1;" == ("","x=4")
+-- testParser "x := 0 - 2;" == ("","x=-2")
+-- testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
+-- testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1")
+-- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
+-- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
+-- testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("","x=34,y=68")
+-- testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
+-- testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1")
+-- testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
+-- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
+-- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
